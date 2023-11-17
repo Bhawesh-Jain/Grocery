bcb233b6ccf4b4ce70873489a5243205e3b8f48a
Bhawesh <bhaweshjain384@gmail.com>
2023-11-17 13:35:15 +0530

    Fixed Create User

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -11,48 +11,56 @@ async function createUser(req, res) {
     var fcm_token = req.body.fcm_token;
 
     if (userId && userId.length > 0 && password && password.length > 0) {
-        // Validate and sanitize inputs
-        if (!name) name = "";
-        if (!phone) phone = "";
-        if (!fcm_token) fcm_token = "";
+        const user = await User.where({ userId: userId }).findOne();
 
-        const saltRounds = 10;
+        if (user) {
+            res.send(JSON.stringify({
+                "result": "false",
+                "msg": "UserId Already Exists!"
+            }));
+        } else {
+            // Validate and sanitize inputs
+            if (!name) name = "";
+            if (!phone) phone = "";
+            if (!fcm_token) fcm_token = "";
 
-        bcrypt.genSalt(saltRounds, (err, salt) => {
-            if (err) {
-                console.error('Error generating salt:', err);
-                res.status(500).json({ "result": "false", "message": "Server Error" });
-            } else {
-                bcrypt.hash(password, salt, async (err, hash) => {
-                    if (err) {
-                        console.error('Error hashing password:', err);
-                        res.status(500).json({ "result": "false", "message": "Server Error" });
-                    } else {
-                        try {
-                            const data = new User({
-                                userId: userId,
-                                password: hash,
-                                name: name,
-                                phone: phone,
-                                userType: "employee",
-                                FCM_TOKEN: fcm_token
-                            });
+            const saltRounds = 10;
 
-                            const dataToSave = await data.save();
-                            res.json({
-                                "result": "true",
-                                "msg": "New User Created",
-                                data: dataToSave
-                            });
-                        } catch (error) {
-                            console.error('Error saving user to database', error);
-                            res.status(400).json({ "result": "false", "message": "Server Error" });
+            bcrypt.genSalt(saltRounds, (err, salt) => {
+                if (err) {
+                    console.error('Error generating salt:', err);
+                    res.status(500).json({ "result": "false", "message": "Server Error" });
+                } else {
+                    bcrypt.hash(password, salt, async (err, hash) => {
+                        if (err) {
+                            console.error('Error hashing password:', err);
+                            res.status(500).json({ "result": "false", "message": "Server Error" });
+                        } else {
+                            try {
+                                const data = new User({
+                                    userId: userId,
+                                    password: hash,
+                                    name: name,
+                                    phone: phone,
+                                    userType: "employee",
+                                    FCM_TOKEN: fcm_token
+                                });
+
+                                const dataToSave = await data.save();
+                                res.json({
+                                    "result": "true",
+                                    "msg": "New User Created",
+                                    data: dataToSave
+                                });
+                            } catch (error) {
+                                console.error('Error saving user to database', error);
+                                res.status(400).json({ "result": "false", "message": "Server Error" });
+                            }
                         }
-                    }
-                });
-            }
-        }); 
-
+                    });
+                }
+            });
+        }
     } else {
         res.send(JSON.stringify({
             "result": "false",
