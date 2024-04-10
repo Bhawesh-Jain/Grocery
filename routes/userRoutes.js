4451cd1764d5bf04ddf2ad15b17dba826b04998a
Bhawesh <bhaweshjainskype@gmail.com>
2024-04-10 14:42:53 +0530

    Initialization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const { login, getProfile, getTaskList, attendanceUpdate } = require('../controllers/userController')
 
 const app = express();
 
@@ -9,14 +8,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 router.use(bodyParser.json())
     
-router.get('/getTaskList', (req, res) => getTaskList(req, res))
-    
-router.post('/login', (req, res) => login(req, res))
-router.post('/getProfile', (req, res) => getProfile(req, res))
-router.post('/attendanceIn', (req, res) => attendanceUpdate(req, res, 1))
-router.post('/attendanceOut', (req, res) => attendanceUpdate(req, res, 2))
-
-
 router.get("*", (req, res) => res.send("PAGE NOT FOUND")); 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require("express");
 const userRoutes = require("./routes/userRoutes.js");
-const adminRoutes = require("./routes/adminRoutes.js");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -26,10 +25,8 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 userRoutes.use(bodyParser.json())
-adminRoutes.use(bodyParser.json())
 
 app.use("/user", userRoutes);
-app.use("/admin", adminRoutes);
 
 app.listen(port, () => {
     console.log(`Server started running at Port ${port}`);
